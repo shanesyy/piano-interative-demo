1b17a9ee811418b90ff2631b0826da1c2d29b240
ronggong <ronggong1010@gmail.com>
2019-01-07 23:11:33 +0000

    alignment

---
+++ src_video/transcribe_audio.sh
@@ -0,0 +1,3 @@
+CHECKPOINT_DIR="/Users/ronggong/Documents_using/pianoAI/data/transcription_models/onset_and_frames"
+
+onsets_frames_transcription_transcribe --acoustic_run_dir="${CHECKPOINT_DIR}" $1
